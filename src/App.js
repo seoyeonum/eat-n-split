8002fa8d79113f685c439c73ba49a1135accd6af
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-10-13 22:54:39 +0900

    ✨ feat: add static part of the application
    - add FriendsList and Friend components with array.map() and && operator
    - add FormAddFreind and FromSplitBill components
    - add Button component with children props to increase reusability

--- src/App.js
+++ src/App.js
@@ -20,5 +20,91 @@ const initialFriends = [
 ];
 
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
+      </div>
+      <FormSplitBill />
+    </div>
+  );
+}
+
+function FriendsList() {
+  const freinds = initialFriends;
+  return (
+    <ul>
+      {freinds.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🐣Friend name</label>
+      <input type="text" />
+
+      <label>📷Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill({}) {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🐶 Your expense</label>
+      <input type="text" />
+
+      <label>🐱 X's expense</label>
+      <input type="text" disabled />
+
+      <label>❓ Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Add</Button>
+    </form>
+  );
 }

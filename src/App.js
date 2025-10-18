5cb0eaa0fea836b47b629b74db6d6506c5671154
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-10-19 01:32:11 +0900

    🚑 fix: restrict upper limit of the paidByUser
    - restrict upper limit of the paidByUser larger than the bill

--- src/App.js
+++ src/App.js
@@ -177,7 +177,11 @@ function FormSplitBill({ selectedFriend }) {
       <input
         type="text"
         value={paidByUser}
-        onChange={(e) => setPaidByUser(Number(e.target.value))}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByUser : Number(e.target.value)
+          )
+        }
       />
 
       <label>🐱 {selectedFriend.name}'s expense</label>
